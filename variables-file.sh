9826c6eda018444cbc6efeb10b65fac69b4b3963
Abhilash <abhilash.ramadurgam7@gmail.com>
2025-03-04 23:56:22 +0530

    some-message

--- variables-file.sh
+++ variables-file.sh
@@ -5,6 +5,6 @@ PERSON2=Abhi
 
 
 echo "$PERSON1: Hi $PERSON2, How are you?"
-echo "$PERSON2: Hello $PERSON2, I am fine and You??"
+echo "$PERSON2: Hello $PERSON1, I am fine and You??"
 echo "$PERSON1: How is your work?"
 echo "$PERSON2: Yeah!, not bad. I am thinking to upgrade Devops"
