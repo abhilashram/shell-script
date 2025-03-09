dd0b51d9c175120a86685508a7110a5c6d5c5787
Abhilash <abhilash.ramadurgam7@gmail.com>
2025-03-09 11:13:20 +0530

    some-message

---
+++ 01-funcations.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+USERID=$( id -u)
+
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then 
+        echo "$2.... Failed"
+    else
+        echo "$2.... Success"
+fi 
+    
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "You are root user"
+    exit 1 
+else
+    echo "You are Super user"
+fi 
+
+dnf install mysql -y 
+VALIDATE $? "Installing MYSQL"
+
+
+dnf install git -y 
+VALIDATE $? "Installing Git"
+
