00a5a79bc222c4da0ea02a3205c0a780024da253
Abhilash <abhilash.ramadurgam7@gmail.com>
2025-03-09 12:25:28 +0530

    some-message

---
+++ 01-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R Failed"
+    else
+        echo -e "$2....$G Success"
+    fi 
+    
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo  "You are root user"
+    exit 1 
+else
+    echo  "You are Super user"
+fi 
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "Installing MYSQL"
+
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "Installing Git"
+
+dnf install dockerr -y &>>LOGFILE
+VALIDATE $? "Installing Dockerr"
+
