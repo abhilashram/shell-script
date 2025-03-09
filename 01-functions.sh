d1972d6c5791b8ba52d9e77cced0e8650b4450cf
Abhilash <abhilash.ramadurgam7@gmail.com>
2025-03-09 11:40:40 +0530

    some-message

--- 01-functions.sh
+++ 01-functions.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
-USERID=$( id -u)
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -21,10 +26,10 @@ else
     echo "You are Super user"
 fi 
 
-dnf install mysql -y 
+dnf install mysql -y &>>LOGFILE
 VALIDATE $? "Installing MYSQL"
 
 
-dnf install git -y 
+dnf install git -y &>>LOGFILE
 VALIDATE $? "Installing Git"
 
